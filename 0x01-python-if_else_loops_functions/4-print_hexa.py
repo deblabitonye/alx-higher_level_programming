a61ffc2521cb107b82735f6ffab4533f62127dbd
deblabitonye <bitonyedebra@gmail.com>
2023-11-28 04:30:37 -0800

    4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-# Author - Debla Bitonye
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
+for i in range(0, 99):
+    print("{} = {}".format(i, hex(i)))
