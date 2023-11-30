cd3b4fc52aa7fa7e1f58551ec2401606e4e67d7d
deblabitonye <bitonyedebra@gmail.com>
2023-11-30 02:16:52 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     """print the sum of 1 and 2."""
     from add_0 import add
-    i = 1
-    j = 2
+    a = 1
+    b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
