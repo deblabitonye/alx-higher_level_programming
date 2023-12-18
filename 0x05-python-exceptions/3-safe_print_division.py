cfde466d781ab974b4a2f02dc86364cf748771c8
deblabitonye <bitonyedebra@gmail.com>
2023-12-18 02:29:17 -0800

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@
 def safe_print_division(a, b):
     """Returns the division of a by b."""
     try:
-       div = a / b
+        div = a / b
     except (TypeError, ZeroDivisionError):
         div = None
     finally:
