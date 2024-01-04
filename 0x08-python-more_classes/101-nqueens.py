ed96399a464c2413817ce7e10d915378f62b9352
deblabitonye <bitonyedebra@gmail.com>
2024-01-04 04:52:45 -0800

    101-nqueens.py

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""N-Queens problem"""
+
+
+import sys
+
+
+def print_solution(solution):
+    """Print the chessboard with queens placed."""
+    for row in solution:
+        print([row.index(1), row.index(1)])
+
+
+def is_safe(board, row, col, n):
+    """Check if it's safe to place a queen at board[row][col]."""
+    # Check this row on the left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    # Check upper diagonal on the left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check lower diagonal on the left side
+    for i, j in zip(range(row, n), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens(board, col, n):
+    """Solve the N-Queens problem using backtracking."""
+    if col >= n:
+        print_solution(board)
+        return True
+
+    res = False
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = 1
+
+            res = solve_nqueens(board, col + 1, n) or res
+
+            board[i][col] = 0  # backtrack if placing a queen doesn't lead to a solution
+
+    return res
+
+
+def nqueens(n):
+    """Solve the N-Queens problem for a given board size."""
+    if not isinstance(n, int):
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0] * n for _ in range(n)]
+    if not solve_nqueens(board, 0, n):
+        print("No solution found")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} N".format(sys.argv[0]))
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+        nqueens(n)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
