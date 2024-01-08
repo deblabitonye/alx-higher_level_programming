bd76cf1952523d370517ca8d72336ac11d0ecb14
deblabitonye <bitonyedebra@gmail.com>
2024-01-08 04:49:14 -0800

    1-my_list.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,6 +11,6 @@ def inherits_from(obj, a_class):
         If obj is an inherited instance of a_class - True.
         Otherwise - False.
     """
-    If issubclass(type(obj), a_class) and type(obj) != a_class:
+    If issubclass(type(obj), a_class) and type(obj) != a_class
         return True
     return False
