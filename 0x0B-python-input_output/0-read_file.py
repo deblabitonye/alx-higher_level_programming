c14823e713938c76c4cac32103804a9ab3c1c4bc
deblabitonye <bitonyedebra@gmail.com>
2024-01-09 01:43:08 -0800

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""""defining read_file function"""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
