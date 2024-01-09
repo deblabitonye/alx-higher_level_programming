a0cb2a3c4a888573d24e0a692a997ebf703e0d6d
deblabitonye <bitonyedebra@gmail.com>
2024-01-09 03:18:21 -0800

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''task 10 module'''
+
+
+class Student:
+    """Defines a student with first_name, last_name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name, and age."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance."""
+        if attrs is not None and all(isinstance(attr, str) for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        return self.__dict__
