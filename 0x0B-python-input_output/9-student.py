2c175e4c659dce54d4d0bda4984f7406ba0056ce
deblabitonye <bitonyedebra@gmail.com>
2024-01-09 03:13:33 -0800

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -2,14 +2,15 @@
 """contains the class "student"""
 
 
+
 class student:
     """Representation of a student"""
     def __init__(self, first_name, last_name, age):
-        """Instantiation with first_name, last_name, and age."""
+        """Initializes the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Retrieves a dictionary representation of a Student instance."""
+        """Returns a dictionary representation of a Student instance."""
         return self.__dict__
