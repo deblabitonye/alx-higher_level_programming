061bc25074f1d295d7149ffb5b2f1d431acfd6a9
deblabitonye <bitonyedebra@gmail.com>
2024-01-09 05:00:14 -0800

    12-pascal_triangle.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+'''task 10 module'''
 
 class Student:
-    """Defines a student with first_name, last_name, and age."""
-
+    '''student class'''
     def __init__(self, first_name, last_name, age):
         """Instantiation with first_name, last_name, and age."""
         self.first_name = first_name
