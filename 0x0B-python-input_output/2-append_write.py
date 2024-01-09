be6e01156c6e9d1b27bf85f144d6e938348cb4cb
deblabitonye <bitonyedebra@gmail.com>
2024-01-09 02:05:25 -0800

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,6 @@
 """defining append_write function"""
 
 
-
 def append_write(filename="", text=""):
     """Appends filename with utf-8"""
     with open(filename, mode='a', encoding='utf-8') as file:
